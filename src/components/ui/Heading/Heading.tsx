58ba01a19781d39cd77a7d5880986b2640fe546e
gamenarkyt <gagaritin11@gmail.com>
2023-09-19 18:24:41 +0300

    Add text and children move to right

--- src/components/ui/Heading/Heading.tsx
+++ src/components/ui/Heading/Heading.tsx
@@ -1,10 +1,17 @@
 import { FC, PropsWithChildren } from "react";
 import styles from "./Heading.module.scss";
 
-interface IProps {}
+interface IProps {
+  text: string;
+}
 
-const Heading: FC<PropsWithChildren<IProps>> = ({ children }) => {
-  return <h1 className={styles.heading}>{children}</h1>;
+const Heading: FC<PropsWithChildren<IProps>> = ({ children, text }) => {
+  return (
+    <h1 className={styles.heading}>
+      <span className={styles.text}>{text}</span>
+      <div className={styles.rightside}>{children}</div>
+    </h1>
+  );
 };
 
 export { Heading };
