17b67eadde72892e777214c4ff4f2785409f24d5
omniroot <omnirootofc@gmail.com>
2024-10-02 16:39:11 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "@/router";
 import "@/utils/checkChrome.ts";
 
+// test
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
